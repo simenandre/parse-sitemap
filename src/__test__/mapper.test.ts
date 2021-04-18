fee384b690b5a4b9c8aa09e8621e298440149736
Simen A. W. Olsen <so@bjerk.io>
2021-04-18 16:39:25 +0200

    fix: Update tests

--- src/__test__/mapper.test.ts
+++ src/__test__/mapper.test.ts
@@ -1,13 +1,37 @@
+import nock from 'nock';
 import { parseSiteMapper } from '../mapper';
 
+const siteMap = `
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  <url>
+    <loc>https://bjerk.io/projects/indiv</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://bjerk.io/projects/lent</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+</urlset>
+`;
+
 describe('mapper', () => {
-  it('should parse enterprise.google.com/sitemap.xml', async () => {
-    const res = await parseSiteMapper(
-      'https://enterprise.google.com/sitemap.xml',
-    );
+  it('should parse a sitemap.xml', async () => {
+    const scope = nock('https://bjerk.io')
+      .get('/sitemap.xml')
+      .reply(200, siteMap);
+
+    const res = await parseSiteMapper('https://bjerk.io/sitemap.xml');
     expect(res).toMatchInlineSnapshot(`
       Array [
-        "https://enterprise.google.no/intl/no/manufacturing.html",
+        "https://bjerk.io/projects/indiv",
+        "https://bjerk.io/projects/lent",
       ]
     `);
   });
