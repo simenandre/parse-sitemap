a763be1bef77b3aedce055cfb24eb91243fe1a8b
Simen A. W. Olsen <cobraz@cobraz.no>
2021-04-18 17:24:13 +0200

    feat: Add `replaceDomain` (#10)

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,39 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 import { getInput } from '@actions/core';
 import * as rt from 'runtypes';
+import { invariant } from './utils';
 
 if (!process.env.CI) {
   require('dotenv').config();
 }
 
+export const replaceDomain = rt.Record({
+  from: rt.String,
+  to: rt.String,
+});
+
+export type ReplaceDomainType = rt.Static<typeof replaceDomain>;
+
 export const config = rt.Record({
   siteMapURL: rt.String,
+  replaceDomain: replaceDomain.optional(),
 });
+
+function parseReplaceDomain(replaceStr?: string): undefined | ReplaceDomainType {
+  if (!replaceStr) return undefined;
+  const [from, to] = replaceStr.split('|');
+  invariant(
+    from || to,
+    'missing from or to. Syntax for replaceDomain is: fromdomain.tld|todomain.tld',
+  );
+  return replaceDomain.check({ from, to });
+}
 
 export type Config = rt.Static<typeof config>;
 
 export async function makeConfig(): Promise<Config> {
   return config.check({
     siteMapURL: getInput('sitemap-url'),
+    replaceDomain: parseReplaceDomain(getInput('replace-domain')),
   });
 }
