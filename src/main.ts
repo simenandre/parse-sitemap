c4b36c797074ab5dfaad0a2b004b87489cad00ca
Simen A. W. Olsen <so@bjerk.io>
2021-04-18 17:21:23 +0200

    feat: Add `replaceDomain`

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { debug, setFailed, setOutput } from '@actions/core';
 import { makeConfig } from './config';
 import { parseSiteMapper } from './mapper';
+import { invariant } from './utils';
 
 export async function main(): Promise<void> {
   const config = await makeConfig();
   const urls = await parseSiteMapper(config.siteMapURL);
-  setOutput('urls', urls.join('\n'));
+
+  let output = urls.join('\n');
+
+  if (config.replaceDomain) {
+    invariant(config.replaceDomain, 'expect replaceDomain');
+    const { from, to } = config.replaceDomain;
+    output = output.replace(from, to);
+  }
+
+  setOutput('urls', output);
 }
 
 main().catch(e => {
